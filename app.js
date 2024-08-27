4a06ce2ec04f3a5b4da339317da7cf679c92ceca
Cédric LEVY <levycedric@hotmail.fr>
2024-08-27 03:11:19 +0200

    ast

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var categoriesRouter = require('./routes/categories');
 var app = express();
 
 app.use(cors({
-  origin:'*',
+  origin: 'https://projet-findebatch-frontend.vercel.app/',
   credentials: true}))
 app.use(logger('dev'));
 app.use(express.json());

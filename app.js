8fe76a626dd4d6f6fde03b12e631fdcd0e925e57
Cédric LEVY <levycedric@hotmail.fr>
2024-08-28 11:11:28 +0200

    ok

--- app.js
+++ app.js
@@ -7,17 +7,18 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fileUpload = require('express-fileupload');
 
-var app = express();
-app.use(cors({
-  origin: '*',
-  credentials: true}))
-
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var placesRouter = require('./routes/places');
 var eventsRouter = require('./routes/events');
 var categoriesRouter = require('./routes/categories');
+
+
+var app = express();
+app.use(cors({
+  origin: 'https://projet-findebatch-frontend.vercel.app',
+  credentials: true}))
 
 
 app.use(logger('dev'));
